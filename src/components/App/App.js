e1f542f3468273e8627530242b48eeefe3d2b702
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-22 22:17:17 +0200

    initialize

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BookFinder from '../Book_finder/Book_finder'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BookFinder/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Book_finder/Book_finder.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Book_finder.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
+function BookFinder() {
+  return (
+    <div className="BookFinder">
+        <h1>Search for book</h1>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="contained" color="primary">
+            Search
+        </Button>
+    </div>
+  );
+}
+
+export default BookFinder;
