ad8fc624b49caceed353e1b751094a18d10a50da
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-25 12:53:52 +0200

    add result list component and connect it to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import './App.css';
-import BookFinder from '../Book_finder/Book_finder'
+import BookFinder from '../Book_finder/Book_finder';
 
+const LazyResultListComponent = React.lazy(()=>import('../Result_list/Result_list.js'));
 
 function App() {
   return (
     <div className="App">
       <BookFinder/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyResultListComponent/>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Result_list/Result_list.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Result_list.scss';
+class ResultList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default ResultList;
