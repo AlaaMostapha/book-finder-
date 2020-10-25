ff05be04f09b9baa33ac3fc04d67020a3723bd8d
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-25 15:37:29 +0200

    working on api

--- src/axios/api/book_api.js
+++ src/axios/api/book_api.js
@@ -1,3 +1,18 @@
-const searchBookApi ="AIzaSyAXDdnulyGAc4aLzIcnIEuHzCm9iadGA6g";
 
-export default searchBookApi;
+import axiosInstance from '../axios/book_find_axios';
+const searchBookApi ="AIzaSyAXDdnulyGAc4aLzIcnIEuHzCm9iadGA6g";
+//get books list
+// export const getBooks = async (book) =>{
+//     try{
+//         await axiosInstance.get(book+"&key="+searchBookApi).then(
+//             data=>{
+//                 console.log(data)
+//             }
+//         )
+//     }catch(error){
+//         console.error(error)
+//     }
+// }
+export const getBooks = async (params) => {
+  return await axiosInstance.get(params+"&key="+searchBookApi);
+};
