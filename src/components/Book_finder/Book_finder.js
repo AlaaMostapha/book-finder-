1f562fd8df606c7c33fd9f06ce2c2fdf94296eea
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-26 15:31:25 +0200

    adding new material ui elements

--- src/components/Book_finder/Book_finder.js
+++ src/components/Book_finder/Book_finder.js
@@ -3,11 +3,13 @@ import React from "react";
 import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import TextField from "@material-ui/core/TextField";
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
+import { Box } from '@material-ui/core'
 import "./Book_finder.scss";
 import store from '../../redux/store/store';
-import  { GET_BOOKS_REUEST,BOOKS_RESPONSE} from '../../constants/actionTypes';
+import  { GET_BOOKS_REUEST} from '../../constants/actionTypes';
 
 const SearchSchema = Yup.object().shape({
   search: Yup.string()
@@ -17,8 +19,17 @@ const SearchSchema = Yup.object().shape({
 const initialValues = {
   search: "",
 };
-
+const useStyles = makeStyles({
+  btn: {
+    // padding: '0 30px',
+    margin: '5px',
+  },
+  textCenter:{
+     textAlign: 'center',
+  }
+});
 function BookFinder() {
+  const classes = useStyles();
   const submitForm = (values) => {
     console.log(values);
     store.dispatch({
@@ -27,13 +38,21 @@ function BookFinder() {
         value:values.search
       }
     })
+   
   };
   const renderForm = (props) =>{
-    console.log(props);
+      
+    // console.log(props);
     const {handleSubmit,handleChange,touched,handleBlur,isValid,dirty,errors} = props;
     return(
-      <div className="container">
-              <h1>Search for your Book</h1>
+      <div>
+            <h1 className={classes.textCenter}>Search for your Book</h1>
+      <Grid   
+        container
+        direction="row"
+        justify="center"
+        alignItems="center">
+          
               <Form onSubmit={handleSubmit}>
                 <div className="form-row">
                   <label htmlFor="search">Search: </label>
@@ -58,13 +77,14 @@ function BookFinder() {
                       <div>{errors.search}</div>
                     )} */}
                   </div>
-                  <div>
+                  <div className={classes.textCenter}>
                     <Button
                       type="submit"
                       variant="contained"
                       color="primary"
                       // className={classes.btn}
-                      className={dirty && isValid ? "" : "disabled-btn"}
+                       className={`${classes.btn} ${dirty && isValid ? "" : "disabled-btn"}`}
+                      // className={dirty && isValid ? "" : "disabled-btn"}
                       disabled={!(dirty && isValid)}
                     >
                       Search
@@ -72,7 +92,9 @@ function BookFinder() {
                   </div>
                 </div>
               </Form>
-            </div>
+            
+    </Grid>
+      </div>
     )
   }
 
