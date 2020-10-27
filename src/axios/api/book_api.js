bdbf1722f41a94caa8e993b4ff758c5f45cfd252
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-27 15:37:03 +0200

    remove unneeded code

--- src/axios/api/book_api.js
+++ src/axios/api/book_api.js
@@ -1,18 +1,7 @@
 
 import axiosInstance from '../axios/book_find_axios';
 const searchBookApi ="AIzaSyAXDdnulyGAc4aLzIcnIEuHzCm9iadGA6g";
-//get books list
-// export const getBooks = async (book) =>{
-//     try{
-//         await axiosInstance.get(book+"&key="+searchBookApi).then(
-//             data=>{
-//                 console.log(data)
-//             }
-//         )
-//     }catch(error){
-//         console.error(error)
-//     }
-// }
+
 export const getBooksFromApi = async (params) => {
   return await axiosInstance.get("books/v1/volumes?q="+params+"&key="+searchBookApi);
 };
