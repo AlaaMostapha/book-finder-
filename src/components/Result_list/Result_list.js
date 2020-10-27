d55ec810053326162de73f88eb24aa0445d35402
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-27 13:55:05 +0200

    add useState hook to get data

--- src/components/Result_list/Result_list.js
+++ src/components/Result_list/Result_list.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Result_list.scss';
 import store from '../../redux/store/store';
 import * as actions from '../../redux/actions/index';
 
-class ResultList extends Component {
-    // constructor(props){
-    //     super(props);
+function ResultList() {
+  let [BookList, setBookList] = useState([]);
 
-    // }
-    componentDidMount=()=>{
-         store.dispatch(actions.addBooks());
+   function load(){
+        store.dispatch(actions.addBooks());
          console.log(actions.addBooks());
          console.log(store.getState())
-    }
-    
-    
-    render() { 
+         BookList=store.getState()
+    setBookList(BookList)
+        //  this.setState({ books: store.getState() });
+         console.log(BookList)
+    } 
+ 
         return ( 
             <React.Fragment>
-
+<button onClick={load}>
+  click
+</button>
+{/* {BookList.map(book=>(
+           <div key={book.payload}>{book}</div>
+         ))} */}
             </React.Fragment>
          );
     }
-}
+
 export default ResultList;
