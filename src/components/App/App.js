fb9c07480556897511859bb736df6a4c21f5960a
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-28 15:22:54 +0200

    enhance app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,19 @@
-import React,{Suspense} from 'react';
+import React from 'react';
 import './App.scss';
 import BookFinder from '../Book_finder/Book_finder';
 import { Provider } from 'react-redux';
 import store from '../../redux/store/store'
-const LazyResultListComponent = React.lazy(()=>import('../Result_list/Result_list.js'));
+import BooksReultList from '../Result_list/Result_list.js'
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      <BookFinder/>
-      <Suspense fallback={<div>Loading.......</div>}>
-        
-        <LazyResultListComponent/>
-      </Suspense>
-   
-    </div>
+      <div className="App">
+        <BookFinder/>
+        <BooksReultList/>   
+      </div>
   </Provider>
   );
 }
 
-// export default App;
 export default App;
