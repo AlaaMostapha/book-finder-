f975e3566a0c9552a69310a394e289bb7124bfa1
AlaaMostapha <alaamoustapha21@gmail.com>
2020-10-28 17:58:20 +0200

    enhance style of grid and add lazy loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import './App.scss';
 import BookFinder from '../Book_finder/Book_finder';
 import { Provider } from 'react-redux';
 import store from '../../redux/store/store'
-import BooksReultList from '../Result_list/Result_list.js'
+const LazyResultListComponent = React.lazy(()=>import('../Result_list/Result_list.js'));
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <BookFinder/>
-        <BooksReultList/>   
-      </div>
+    <div className="App">
+      <BookFinder/>
+      <Suspense fallback={<div>Loading.......</div>}>
+        <LazyResultListComponent/>
+      </Suspense>
+   
+    </div>
   </Provider>
   );
 }
 
+// export default App;
 export default App;
--- src/components/Result_list/Result_list.js
+++ src/components/Result_list/Result_list.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import './Result_list.scss';
 import {connect} from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 class  ResultList extends Component {
 
@@ -10,19 +12,26 @@ class  ResultList extends Component {
       if(this.props.books) { 
         return (this.props.books.items || []).map((book)=>{
          return(
-         <div key={book.id}>
-           <h3>{book.volumeInfo.title}</h3> 
-           <p>{book.kind}</p>
-           <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.kind}/>
-            </div>
+           <Grid item xs={3}>
+             <Paper className="paper-custom">
+              <div key={book.id}>
+                  <h3>{book.volumeInfo.title}</h3> 
+                  <p>{book.kind}</p>
+                  <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.kind}/>
+                </div>
+            </Paper>
+            </Grid>
          )
        })
       }
+      
   }
  render() { 
   return ( 
     <React.Fragment>
+      <Grid container spacing={2} alignItems="stretch" m="2rem">
         {this.createList()}
+        </Grid>
     </React.Fragment>
   );
   }
