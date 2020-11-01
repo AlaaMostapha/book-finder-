8a4b48d52a585a61b7cc6b7ac6db3284b236a88c
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-01 17:49:30 +0200

    add loading new feature to interceptors

--- src/axios/handlers/index.js
+++ src/axios/handlers/index.js
@@ -1,12 +1,23 @@
+import * as actions from '../../redux/actions/index';
 export const requestHandler = (request) => {
-  console.log(`request handler${request}`);
+  console.log(`request handler ${request}`);
+
+  actions.startLoading("true")
+    console.log(request)
+  // document.body.classList.add('loading-indicator');
+  // document.getElementById('loader3').show();
   return request
 }
 export const errorHandler = (error) => {
-   console.log(`error handler${error}`);
+   console.log(`error handler ${error}`);
+     console.log(error)
   return Promise.reject({ ...error })
 }
 export const successHandler = (response) => {
-   console.log(`response handler${response}`);
+   console.log(`response handler ${response}`);
+   console.log(response)
+   actions.endLoading("false")
+  //  document.getElementById('loader3').hide();
+  //  document.body.classList.remove('loading-indicator');
   return response
 }
--- src/axios/interceptors/index.js
+++ src/axios/interceptors/index.js
@@ -1,14 +1,14 @@
-import axiosInstance from '../axios/book_find';
-import * as Handlers from '../handlers/index';
+// import axiosInstance from '../axios/book_find';
+// import * as Handlers from '../handlers/index';
 
-//request interceptor
-axiosInstance.interceptors.request.use(
- request => Handlers.requestHandler(request)
-);
+// //request interceptor
+// axiosInstance.interceptors.request.use(
+//  request => Handlers.requestHandler(request)
+// );
 
-//response interceptor
-axiosInstance.interceptors.response.use(
-  response => Handlers.successHandler(response),
-  error => Handlers.errorHandler(error)
-);
+// //response interceptor
+// axiosInstance.interceptors.response.use(
+//   response => Handlers.successHandler(response),
+//   error => Handlers.errorHandler(error)
+// );
 
