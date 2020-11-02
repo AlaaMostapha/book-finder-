f961025bf221d38443e897a562f8aba0d3e80dd2
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-02 16:26:43 +0200

    add loading action

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,4 +1,5 @@
 export const BOOKS_REUEST='BOOKS_REUEST';
 export const BOOKS_RESPONSE='BOOKS_RESPONSE';
-export const LOAD_START='LOAD_START';
-export const LOAD_END='LOAD_END';
+// export const LOAD_START='LOAD_START';
+// export const LOAD_END='LOAD_END';
+export const LOADING='LOADING';
