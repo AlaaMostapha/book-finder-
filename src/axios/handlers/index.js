790657009cbeac34f1efa452f1da56d1d5f4277f
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-02 16:23:18 +0200

    sollve dispatch action in interceptors

--- src/axios/handlers/index.js
+++ src/axios/handlers/index.js
@@ -1,23 +1,28 @@
 import * as actions from '../../redux/actions/index';
+import store from '../../redux/store/store'
 export const requestHandler = (request) => {
   console.log(`request handler ${request}`);
+  console.log(request)
+  store.dispatch(actions.loading(true))
+  console.log(store.dispatch(actions.loading(true)))
 
-  actions.startLoading("true")
-    console.log(request)
   // document.body.classList.add('loading-indicator');
-  // document.getElementById('loader3').show();
   return request
 }
 export const errorHandler = (error) => {
-   console.log(`error handler ${error}`);
-     console.log(error)
+  console.log(`error handler ${error}`);
+  store.dispatch(actions.loading(false))
+  console.log(error)
+
+  //  document.body.classList.remove('loading-indicator');
   return Promise.reject({ ...error })
 }
 export const successHandler = (response) => {
-   console.log(`response handler ${response}`);
-   console.log(response)
-   actions.endLoading("false")
-  //  document.getElementById('loader3').hide();
+  console.log(`response handler ${response}`);
+  console.log(response)
+  store.dispatch(actions.loading(false))
+  console.log(store.dispatch(actions.loading(false)))
+
   //  document.body.classList.remove('loading-indicator');
   return response
 }
