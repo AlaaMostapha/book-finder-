0f6bb21d2cfefc6f90fffd96c2c08884687bf2ef
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-02 16:24:02 +0200

    add loading component

--- src/components/Loading_indicator/Loading_indicator.js
+++ src/components/Loading_indicator/Loading_indicator.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React ,{Component}from 'react';
+import './Loading_indicator.scss';
 import Loader from 'react-loader-spinner';
 
-const LoadingIndicator = props => {
+
+class  LoadingIndicator extends Component{
+ render() { 
   return(
-    <div className="loader">
-      <Loader type="Bars" color="#3f51b5" secondaryColor="Grey" height="100" width="100" visible={props.loading} />
+    <div className="loader center">
+      <Loader type="Bars" color="#3f51b5" secondaryColor="Grey" height={100} width={100} />
     </div>
   )
+   }
 };
-export default LoadingIndicator;
+
+
+export default LoadingIndicator; 
