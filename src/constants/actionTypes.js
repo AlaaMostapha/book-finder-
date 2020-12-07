9acb46ba6a43056bc15679953a90b82f4263bfb9
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-07 10:23:05 +0200

    remove un needed code (reducer-console logs-commented code)

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,3 @@
-export const BOOKS_REUEST='BOOKS_REUEST';
-export const BOOKS_RESPONSE='BOOKS_RESPONSE';
-// export const LOAD_START='LOAD_START';
-// export const LOAD_END='LOAD_END';
-export const LOADING='LOADING';
+export const BOOKS_REUEST = "BOOKS_REUEST";
+export const BOOKS_RESPONSE = "BOOKS_RESPONSE";
+export const LOADING = "LOADING";
